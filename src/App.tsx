6398942b37d0df8ff2e9b3ca0af44e201c5e6e61
Blessdbnjmn <113176355+Blessdcode@users.noreply.github.com>
2025-08-06 19:57:53 +0100

    Update App

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import FormInput from "./components/form.input";
 
 const App = () => {
   return (
-    <div className=" flex flex-col items-center justify-center min-w-full ">
+    <div className=" flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-indigo-900 via-purple-900 to-pink-900">
       <div>
         <FormInput />
       </div>
